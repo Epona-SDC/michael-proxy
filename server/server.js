a3a5726583fb406de6bfcd069e48128934c6614d
Michael Johnson <hack.reactor.mj0730@gmail.com>
2020-04-14 16:45:08 -0500

    cleanup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const cors = require('cors');
-const port = 3000;
-let app = express();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
 app.use(express.static(__dirname + '/../public'));
-app.listen(port);
+
+app.listen(PORT, console.log(`Proxy server listening on ${PORT}`));
